c32b8c2f7842fcbdbe72ca68da58b0a8c1f35945
unknown <kevinh21@gmail.com>
2022-07-16 11:21:22 -0700

    uploading Final routes version 5 with .zip file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//on cli enter export NODE_OPTIONS=--openssl-legacy-provider to reslove
+//  -- Error: error:0308010C:digital envelope routines::unsupported
+
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
